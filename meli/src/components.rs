dba5b68be8ede3b81eb3926bb5fb6affd9efc436
Manos Pitsidianakis <manos@pitsidianak.is>
2024-10-24 12:22:29 +0300

    components: add prelude module
    Add prelude module with exports that are commonly necessary for
    Component types.
    
    Signed-off-by: Manos Pitsidianakis <manos@pitsidianak.is>

--- meli/src/components.rs
+++ meli/src/components.rs
@@ -318,3 +318,28 @@ impl ComponentPath {
         self.tail.last()
     }
 }
+
+pub mod prelude {
+    pub use crate::components::*;
+    pub use crate::{
+        accounts::MailboxEntry,
+        command::*,
+        components::{
+            Component, ComponentAttr, ComponentId, ComponentPath, ExtendShortcutsMaps,
+            ScrollContext,
+        },
+        jobs::{JobId, JobMetadata},
+        melib::text::TextProcessing,
+        melib::{utils::datetime, SortOrder},
+        shortcut, AccountHash, Action,
+        Action::*,
+        Area, Attr, CellBuffer, Context, DataColumns, EnvelopeHash, Key, MailboxHash, Shortcuts,
+        StatusEvent,
+        TabAction::*,
+        ThemeAttribute, UIDialog, UIEvent, UIMode,
+    };
+
+    pub use std::borrow::Cow;
+
+    pub use indexmap::IndexMap;
+}
