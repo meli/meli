d274fb4b02deb5b7125225179ffec401cc61889d
Manos Pitsidianakis <manos@pitsidianak.is>
2026-08-06 09:42:34 +0300

    melib/notmuch: add integration test for config
    Add an integration test that checks for possible config errors.
    
    Signed-off-by: Manos Pitsidianakis <manos@pitsidianak.is>

--- melib/tests/integration/configs.rs
+++ melib/tests/integration/configs.rs
@@ -115,3 +115,169 @@ rename_regex = ',U=\d\d*'
     melib::maildir::MaildirType::validate_config(&mut s).unwrap();
     _ = tmp_dir.close();
 }
+
+#[cfg(feature = "notmuch")]
+#[test]
+fn test_notmuch_config() {
+    use melib::{
+        error::{Error, ErrorKind},
+        notmuch::NotmuchDb,
+    };
+    use tempfile::TempDir;
+
+    let tmp_dir = TempDir::new().unwrap();
+
+    let root_mailbox = tmp_dir.path().join("root_mailbox");
+    let account_name = "foo";
+
+    let mut s: melib::AccountSettings = toml::from_str(&format!(
+        r#"
+name = "{account_name}"
+root_mailbox = "{}"
+format = "notmuch"
+identity = "foo@example.com"
+    "#,
+        root_mailbox.display()
+    ))
+    .unwrap();
+
+    let notmuch_db_dir = root_mailbox.join(".notmuch");
+
+    assert_eq!(
+        NotmuchDb::validate_config(&mut s).unwrap_err(),
+        Error::new(format!(
+            "Notmuch `root_mailbox` {} for account {account_name} does not exist.",
+            root_mailbox.display()
+        ))
+        .set_related_path(Some(root_mailbox.clone()))
+        .set_kind(ErrorKind::Configuration)
+    );
+    std::fs::write(&root_mailbox, b"foo").unwrap();
+    assert_eq!(
+        NotmuchDb::validate_config(&mut s).unwrap_err(),
+        Error::new(format!(
+            "Notmuch `root_mailbox` {} for account {account_name} is not a directory.",
+            root_mailbox.display()
+        ))
+        .set_related_path(Some(root_mailbox.clone()))
+        .set_kind(ErrorKind::Configuration)
+    );
+    std::fs::remove_file(&root_mailbox).unwrap();
+    std::fs::create_dir(&root_mailbox).unwrap();
+    assert_eq!(
+        NotmuchDb::validate_config(&mut s).unwrap_err(),
+        Error::new(format!(
+            "Notmuch `root_mailbox` {} for account {account_name} does not contain a `.notmuch` \
+             subdirectory.",
+            root_mailbox.display()
+        ))
+        .set_related_path(Some(notmuch_db_dir.clone()))
+        .set_kind(ErrorKind::Configuration)
+    );
+    std::fs::create_dir(&notmuch_db_dir).unwrap();
+    assert_eq!(
+        NotmuchDb::validate_config(&mut s).unwrap_err(),
+        Error::new(format!(
+            "Notmuch account `{account_name}` requires mailboxes explicitly set, since they are \
+             virtual, but none are configured. Try adding some.",
+        ))
+        .set_kind(ErrorKind::Configuration)
+    );
+
+    // Check for missing "query" settings.
+    let mut s: melib::AccountSettings = toml::from_str(&format!(
+        r#"
+name = "{account_name}"
+root_mailbox = "{}"
+format = "notmuch"
+identity = "foo@example.com"
+mailboxes = {{ "INBOX" = {{}}, "Drafts" = {{ query="tag:draft" }}}}
+    "#,
+        root_mailbox.display()
+    ))
+    .unwrap();
+
+    assert_eq!(
+        NotmuchDb::validate_config(&mut s).unwrap_err(),
+        Error::new(format!(
+            "notmuch mailbox configuration entry `INBOX` for account {account_name} should have a \
+             `query` value set."
+        ))
+        .set_kind(ErrorKind::Configuration)
+    );
+
+    // Check for invalid "parent" setting.
+    let mut s: melib::AccountSettings = toml::from_str(&format!(
+        r#"
+name = "{account_name}"
+root_mailbox = "{}"
+format = "notmuch"
+identity = "foo@example.com"
+mailboxes = {{ "INBOX" = {{ query="tag:inbox", parent = "doesnotexist" }}, "Drafts" = {{ query="tag:draft" }}}}
+    "#,
+        root_mailbox.display()
+    ))
+    .unwrap();
+
+    assert_eq!(
+        NotmuchDb::validate_config(&mut s).unwrap_err(),
+        Error::new(
+            "Mailbox configuration for `INBOX` defines its parent mailbox as `doesnotexist` but \
+             no mailbox exists with this exact name."
+        )
+        .set_kind(ErrorKind::Configuration)
+    );
+
+    // Check for "parent" cycle
+    let mut s: melib::AccountSettings = toml::from_str(&format!(
+        r#"
+name = "{account_name}"
+root_mailbox = "{}"
+format = "notmuch"
+identity = "foo@example.com"
+mailboxes = {{ "INBOX" = {{ query="tag:inbox", parent = "Drafts" }}, "Drafts" = {{ query="tag:draft", parent="INBOX"}}}}
+    "#,
+        root_mailbox.display()
+    ))
+    .unwrap();
+
+    assert_eq!(
+        NotmuchDb::validate_config(&mut s).unwrap_err(),
+        Error::new("Found cycle in mailbox hierarchy: INBOX->Drafts")
+            .set_kind(ErrorKind::Configuration)
+    );
+
+    let mut s: melib::AccountSettings = toml::from_str(&format!(
+        r#"
+name = "{account_name}"
+root_mailbox = "{}"
+format = "notmuch"
+identity = "foo@example.com"
+mailboxes = {{ "INBOX" = {{ query="tag:inbox", parent = "Drafts" }}, "Drafts" = {{ query="tag:draft", parent="Sent"}}, "Sent" = {{ query="tag:sent", parent = "INBOX" }} }}
+    "#,
+        root_mailbox.display()
+    ))
+    .unwrap();
+
+    assert_eq!(
+        NotmuchDb::validate_config(&mut s).unwrap_err(),
+        Error::new("Found cycle in mailbox hierarchy: INBOX->Drafts->Sent")
+            .set_kind(ErrorKind::Configuration)
+    );
+
+    // Check valid configuration
+    let mut s: melib::AccountSettings = toml::from_str(&format!(
+        r#"
+name = "{account_name}"
+root_mailbox = "{}"
+format = "notmuch"
+identity = "foo@example.com"
+mailboxes = {{ "INBOX" = {{  query="tag:inbox", subscribe = true }}, "Drafts" = {{ query="tag:draft", subscribe = true }}, "Sent" = {{ query="from:username@example.com from:username2@example.com", subscribe = true }}}}
+    "#,
+        root_mailbox.display()
+    ))
+    .unwrap();
+    NotmuchDb::validate_config(&mut s).unwrap();
+
+    _ = tmp_dir.close();
+}
